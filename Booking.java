0bd60ac0ea49dd180c1a064a7411378f7357ad11
Rehxa <pichsokreaksa505@gmail.com>
2025-01-29 21:58:14 +0700

    ADD new class

---
+++ Booking.java
@@ -0,0 +1,21 @@
+public class Booking {
+    private int bookingId;
+    private int numberOfSeats;
+    private double totalPrice;    
+    private User user;
+    private Hall hall;
+    private Movie movie;
+    private Seat seat;
+    private Payment payment;
+
+    public Booking(int bookingId,int numberOfSeats, double totalPrice, Hall hall, Movie movie, User user, Seat seat,Payment payment) {
+        this.bookingId = bookingId;
+        this.numberOfSeats = numberOfSeats;
+        this.totalPrice = totalPrice;
+        this.user =  user;
+        this.hall =  hall;
+        this.movie = movie;
+        this.seat =  seat;
+        this.payment =  payment;
+    }
+}
---
+++ Showtime.java
@@ -0,0 +1,13 @@
+public class Showtime {
+    private String showTimeID;
+    private String date;
+    private String time;
+    private double pricePerTicket;
+
+    public ShowTime(String showTimeID, String date, String time, double pricePerTicket) {
+            this.showTimeID = showTimeID;
+            this.date = date;
+            this.time = time;
+            this.pricePerTicket = pricePerTicket;
+    }
+}
