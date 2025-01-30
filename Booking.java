d438a99eaaf82eb3630ccd437b1c5b539f4fcd48
Rehxa <pichsokreaksa505@gmail.com>
2025-01-31 00:03:41 +0700

    Add some attributes and change ShowTime name

--- Booking.java
+++ Booking.java
@@ -1,17 +1,19 @@
 public class Booking {
     private int bookingId;
     private int numberOfSeats;
-    private double totalPrice;    
+    private double totalPrice;
+    private String bookingTime;    
     private User user;
     private Hall hall;
     private Movie movie;
     private Seat seat;
     private Payment payment;
 
-    public Booking(int bookingId,int numberOfSeats, double totalPrice, Hall hall, Movie movie, User user, Seat seat,Payment payment) {
+    public Booking(int bookingId,int numberOfSeats, double totalPrice, String bookingTime, Hall hall, Movie movie, User user, Seat seat,Payment payment) {
         this.bookingId = bookingId;
         this.numberOfSeats = numberOfSeats;
         this.totalPrice = totalPrice;
+        this.bookingTime = bookingTime;
         this.user =  user;
         this.hall =  hall;
         this.movie = movie;
--- Showtime.java
+++ Showtime.java
@@ -1,13 +1,17 @@
-public class Showtime {
-    private String showTimeID;
+public class ShowTime {
+    private int showTimeID;
     private String date;
     private String time;
     private double pricePerTicket;
+    private String startTime;
+    private String endTime;
 
-    public Showtime(String showTimeID, String date, String time, double pricePerTicket) {
-            this.showTimeID = showTimeID;
-            this.date = date;
-            this.time = time;
-            this.pricePerTicket = pricePerTicket;
+    public ShowTime(int showTimeID, String date, String time, double pricePerTicket, String startTime, String endTime) {
+        this.showTimeID = showTimeID;
+        this.date = date;
+        this.time = time;
+        this.pricePerTicket = pricePerTicket;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 }
