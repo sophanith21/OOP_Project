ebab9bf6072a0fdb4d551290251d12e016a5bbfc
monica445 <monicashalom86@gmail.com>
2025-01-31 12:27:52 +0700

    add public to constructors and numberOfMovies and resetCode default valu

--- Movie.java
+++ Movie.java
@@ -8,14 +8,14 @@ public class Movie {
     private double rating;
     private ShowTime[] showTimes;
     private int showTimeCount;
-    private static int numberOfMovies = 0;
+    public static int numberOfMovies = 0;
 
-    Movie (String movieID, String hallID, String title, int duration, String genre, int maxShowTimes) {
+    public Movie (String movieID, String hallID, String title, int duration, String genre, int maxShowTimes) {
         this.movieID = movieID;
         this.hallID = hallID;
         this.title = title;
         this.duration = duration;
-        this.genre = genre;
+        this.genre = genre;  
         this.rating = 0.0;
         this.showTimes = new ShowTime[maxShowTimes];
         this.showTimeCount = 0;
