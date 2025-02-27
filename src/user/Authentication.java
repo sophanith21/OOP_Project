249c5a8a81b41eabd0acebd10f7dd44d53e41d91
monica445 <monicashalom86@gmail.com>
2025-02-27 23:49:29 +0700

    Remove authentication class (file)

--- src/user/Authentication.java
+++
@@ -1,9 +0,0 @@
-package src.user;
-
-import java.util.HashMap;
-
-interface Authentication {
-    boolean login(String username, String password);
-    void logout ();
-    boolean register (String name, String phone, String email, String password);
-} 
